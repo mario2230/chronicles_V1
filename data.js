c0d38d5f521c389ba4b9dc1ff4d27e17e44dc8e2
Mario Gonçalves de Freitas Junior <mario.defreitasjr@gmail.com>
2026-07-20 22:09:35 -0300

    update

--- data.js
+++ data.js
@@ -4879,7 +4879,7 @@ const GAME_DATA = {
       nome: "Mercenário Rival",
       tipo: "chefe",
       raridade: "mitica",
-      weight: 9,
+      weight: 50,
       regiaoOrigem: [
         "aldeia",
         "floresta",
