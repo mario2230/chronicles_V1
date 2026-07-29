35b4b7a32c44a334409844a617d120b418a94260
Mario Gonçalves de Freitas Junior <mario.defreitasjr@gmail.com>
2026-07-29 20:27:43 -0300

    update

--- data.js
+++ data.js
@@ -8355,7 +8355,7 @@ const GAME_DATA = {
       "nome": "Elixir dos Deuses Antigos",
       "tipo": "escolha",
       "raridade": "lendaria",
-      "weight": 1,
+      "weight": 0,
       "regiaoOrigem": [
         "ruinas",
         "capital",
